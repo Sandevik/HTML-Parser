6a5218c0613592dd0e6bf015c0e32ce55e619772
[Simon] <[simon.sandevik@outlook.com]>
2023-06-17 14:07:26 +0200

    new plan

--- src/parser/json.rs
+++ src/parser/json.rs
@@ -2,26 +2,83 @@ use std::vec;
 
 use crate::utils::Shift;
 
-#[derive(Debug)]
-pub enum JsonType{
-    String(String),
-    Number(String),
+/* Number */
+/* "{ "a" : 2 }" */
+/* JsonType::Variable({key: "a", value: Some(2)}) */
+
+/* float */
+/* "{ "b" : 2.17 }" */
+/* JsonType::Variable({key: "b", value: Some(2.17)}) */
+
+/* String */
+/* "{ "c" : "x" }" */
+/* JsonType::Variable({key: "c", value: Some("x")}) */
+
+/* Bool */
+/* "{ "d" : true }" */
+/* JsonType::Variable({key: "d", value: Some(true)}) */
+
+/* Null */
+/* "{ "e" : null }" */
+/* JsonType::Variable({key: "e", value: None}) */
+
+/* Array */
+/* "{ "f" : ["x", "y", "z"] }" */
+/* JsonType::Variable(Vec<JsonType::Variable>) */
+
+/* Object */
+/* Recursive */
+
+/* JsonTree */
+/* "{ "a" : 2, "b" : "x", "c": [1, 2, 3]}" */
+/* JsonType::Object(Vec<JsonType>) */
+
+/* Json */
+/*
+    {
+        src: "",
+        parsed: JsonType::Object(Vec<JsonType>)),
+    }
+*/
+
+impl<T> Shift<T> for Vec<T> {
+    fn shift(&mut self) -> ()
+    where
+        T: Clone,
+    {
+        *self = self[1..].to_vec();
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Variable<T> {
+    key: String,
+    value: T,
+}
+
+#[derive(Debug, Clone)]
+pub enum JsonType {
+    String(Variable<String>),
+    Number(Variable<f32>),
     Object(Vec<JsonType>),
     Array(Vec<JsonType>),
     Null,
-    Boolean(bool)
+    Boolean(bool),
 }
 
-
+pub enum JsonParseType {
+    String,
+    Object,
+    Array,
+}
 
 pub struct Json {
     string: String,
-    json_vec: Option<Vec<JsonType>>
-
-} 
+    json_vec: Option<Vec<JsonType>>,
+}
 
 impl Json {
-    pub fn parse(string: String){
+    pub fn parse(string: String) {
         let mut vectorized: Vec<String> = Self::vectorize_string(string);
     }
 
@@ -29,90 +86,16 @@ impl Json {
         let mut bytes: Vec<u8> = string.bytes().collect::<Vec<u8>>();
         let mut vectorized: Vec<String> = Vec::new();
 
-
         while bytes.len() > 0 {
             let mut byte = bytes[0];
-            
-            if byte == b'[' {
-                println!("parse as array");
-                let parsed_arr: Vec<JsonType> = Self::parse_string(&mut bytes, JsonParseType::Array);
 
-                println!("parsed arr: {:?}", JsonType::Array(parsed_arr));
-
-            }else if byte == b'{' {
-                println!("parse as object");
-                let parsed_obj: Vec<JsonType> = Self::parse_string(&mut bytes, JsonParseType::Object);
-                
-                println!("parsed arr: {:?}", JsonType::Object(parsed_obj));
-
-            }else{
-                println!("{:?}", Self::parse_string(&mut bytes, JsonParseType::String));
-            }
-
-            
-        } 
+           
+        }
 
         todo!();
     }
 
-    
-
     fn parse_string(mut bytes: &mut Vec<u8>, parse_as: JsonParseType) -> Vec<JsonType> {
-        bytes.shift();
-        match parse_as {
-            JsonParseType::String => {
-                let mut ident_vec: Vec<JsonType> = Vec::new();
-                let mut ident: String = String::new();
-                while bytes.len() > 0 {
-                    let byte = bytes[0];
-                    if byte == b':' || byte == b',' {
-                        ident_vec.push(JsonType::String(ident.trim().to_string()));
-                    }else if byte == b'{' {
-                        Self::parse_string(&mut bytes, JsonParseType::Object);
-                    } else {
-                        ident.push(byte as char);
-                    }
-                
-                    bytes.shift();
-                }
-                return ident_vec;
-            },
-            JsonParseType::Array => {
-                let mut arr_vec: Vec<JsonType> = Vec::new();
-                let mut arr_item = String::new();
-
-                while bytes.len() > 0 && bytes[0] != b']' {
-                    if bytes[0] == b',' {
-                        arr_vec.push(JsonType::String(arr_item));
-                        arr_item = String::new();
-                    }else{
-                        arr_item.push(bytes[0] as char)
-                    }
-                    bytes.shift();
-                }
-
-                return arr_vec;
-            },
-            JsonParseType::Object => {
-                let obj_vec: Vec<JsonType> = Vec::new();
-
-                return obj_vec;
-            },
-            _ => panic!("This type is not supported")
-        }
+        todo!();
     }
-
-    
-}
-
-impl<T> Shift<T> for Vec<T>{
-    fn shift(&mut self) -> () where T: Clone {
-        *self = self[1..].to_vec();
-    }
-}
-
-pub enum JsonParseType {
-    String,
-    Object,
-    Array,
 }
