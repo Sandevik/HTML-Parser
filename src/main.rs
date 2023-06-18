edc126ced17b8b9b9f3725452aefd3f514730566
[Simon] <[simon.sandevik@outlook.com]>
2023-06-18 20:16:43 +0200

    parse string as variables

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use parser::json::Json;
 
 fn main() {
 
-    let html = String::from("<!DOCTYPE html><html class=\"\" [sdadsa] sadads-fsdaff >content abc 123</html>"); //fs::read_to_string("index.html").expect("File not found");
+    //let html = String::from("<!DOCTYPE html><html class=\"\" [sdadsa] sadads-fsdaff >content abc 123</html>"); //fs::read_to_string("index.html").expect("File not found");
 
-    Json::parse("[ \"x\" : 2, \"y\": 3, \"z\": 4 ]".to_string());
+    println!("{:#?}", Json::parse("{ \"x\" : 2, \"y\": 3, \"z\": \"4\", \"a\" : 3.14, \"b\" : null }".to_string()));
 
     //HTMLParser::extract_tag_name(&"<html class=\"\" class2 assafs [asda]asdads]>".to_string());
 
--- src/parser/json.rs
+++ src/parser/json.rs
@@ -1,35 +1,36 @@
-use std::vec;
+#![allow(dead_code, unused_assignments, unused_mut, unused_variables)]
 use crate::utils::Shift;
+use std::vec;
 
 /* Number */
-/* "{ "a" : 2 }" */
+/* ""a" : 2 " */
 /* JsonType::Variable({key: "a", value: Some(2)}) */
 
 /* float */
-/* "{ "b" : 2.17 }" */
+/* ""b" : 2.17 " */
 /* JsonType::Variable({key: "b", value: Some(2.17)}) */
 
 /* String */
-/* "{ "c" : "x" }" */
+/* ""c" : "x" " */
 /* JsonType::Variable({key: "c", value: Some("x")}) */
 
 /* Bool */
-/* "{ "d" : true }" */
+/* ""d" : true " */
 /* JsonType::Variable({key: "d", value: Some(true)}) */
 
 /* Null */
-/* "{ "e" : null }" */
+/* "e" : null " */
 /* JsonType::Variable({key: "e", value: None}) */
 
 /* Array */
-/* "{ "f" : ["x", "y", "z"] }" */
+/* ""f" : ["x", "y", "z"] " */
 /* JsonType::Variable({key: "f", value: Some(Vec<JsonType)>) */
 
 /* Object */
 /* Recursive */
 
 /* JsonTree */
-/* "{ "a" : 2, "b" : "x", "c": [1, 2, 3]}" */
+/* ""a" : 2, "b" : "x", "c": [1, 2, 3] " */
 /* JsonType::Object(Vec<JsonType>) */
 
 /* Json */
@@ -50,47 +51,136 @@ impl<T> Shift<T> for Vec<T> {
 }
 
 #[derive(Debug, Clone)]
-pub struct Variable<T> {
-    key: String,
-    value: T,
+enum VariableType {
+    String(String),
+    Float(f32),
+    Integer(i32),
+    None
 }
 
 #[derive(Debug, Clone)]
-pub enum JsonType<T> {
-    Variable(Variable<Option<T>>),
-    Object(Vec<JsonType<T>>),
+pub struct Variable {
+    key: String,
+    value: VariableType,
+}
+
+#[derive(Debug, Clone)]
+pub enum JsonType {
+    Variable(Variable),
+    Object(Vec<JsonType>),
+    Array(Vec<JsonType>),
 }
 
 pub enum JsonParseType {
-    String,
     Object,
     Array,
+    Variables,
 }
 
-pub struct Json<T> {
-    src: String,
-    parsed: Option<Vec<JsonType<T>>>,
+pub struct Json {
+    parsed: Vec<JsonType>,
 }
 
-impl<T> Json<T> {
-    pub fn parse(string: String) {
-        let mut vectorized: Vec<String> = Self::vectorize_string(string);
-    }
-
-    fn vectorize_string(string: String) -> Vec<String> {
+impl Json {
+    
+    pub fn parse(string: String) -> Vec<JsonType> {
         let mut bytes: Vec<u8> = string.bytes().collect::<Vec<u8>>();
-        let mut vectorized: Vec<String> = Vec::new();
+        let mut parsed: Vec<JsonType> = Vec::new();
 
         while bytes.len() > 0 {
             let mut byte = bytes[0];
+            if byte == b'{' {
+                bytes.shift();
+                let json_type = Self::parse_string(&mut bytes, JsonParseType::Object);
+                parsed.push(json_type);
+            } else if byte == b'[' {
+                bytes.shift();
+                let json_type = Self::parse_string(&mut bytes, JsonParseType::Array);
+                parsed.push(json_type);
+            }
 
-           
+            bytes.shift();
         }
 
-        vectorized
+        return parsed;
     }
 
-    fn parse_string(mut bytes: &mut Vec<u8>, parse_as: JsonParseType) {
-        todo!("Variableize thing");
+    fn parse_string(mut bytes: &mut Vec<u8>, parse_as: JsonParseType) -> JsonType {
+        match parse_as {
+            JsonParseType::Object => {
+                let mut nested_string = String::new();
+                while bytes.len() > 0 && bytes[0] != b'}' {
+                    nested_string.push(bytes[0] as char);
+                    bytes.shift();
+                }
+                let obj_vec = Self::parse_string(
+                    &mut nested_string.bytes().collect::<Vec<u8>>(),
+                    JsonParseType::Variables,
+                );
+
+                return obj_vec;
+            }
+
+            JsonParseType::Array => {
+                let vec: Vec<JsonType> = Vec::new();
+                let mut nested_string = String::new();
+                while bytes.len() > 0 && bytes[0] != b']' {
+                    nested_string.push(bytes[0] as char);
+                    bytes.shift();
+                }
+                return JsonType::Array(vec);
+            }
+
+            JsonParseType::Variables => {
+                let variables = Self::parse_variables(&mut bytes, None);
+                return variables;
+            }
+        }
+    }
+
+    fn parse_variables(mut bytes: &mut Vec<u8>, return_as: Option<JsonParseType>) -> JsonType {
+        let mut json_type_vec: Vec<JsonType> = Vec::new();
+
+        let string: String = String::from_utf8(bytes.clone()).unwrap();
+        let strs: Vec<&str> = string.split(",").collect::<Vec<&str>>();
+
+        for str in strs {
+            let key = str.split(":").collect::<Vec<&str>>()[0];
+            let value_string = str.split(":").collect::<Vec<&str>>()[1];
+            let mut value: VariableType;
+
+            if value_string.trim() == "null" {
+                value = VariableType::None
+            } else if value_string.contains("\"") {
+                value = VariableType::String(value_string.to_string());
+            } else if value_string.contains(".") {
+                value = VariableType::Float(
+                    value_string
+                        .trim()
+                        .parse::<f32>()
+                        .expect(&format!("Could not parse {}", value_string)),
+                );
+            } else {
+                value = VariableType::Integer(
+                    value_string
+                        .trim()
+                        .parse::<i32>()
+                        .expect(&format!("Could not parse {}", value_string)),
+                )
+            }
+
+            let var: Variable = Variable {
+                key: key.to_string(),
+                value: value,
+            };
+
+            json_type_vec.push(JsonType::Variable(var));
+        }
+
+        return match return_as {
+            Some(JsonParseType::Variables) => JsonType::Array(json_type_vec),
+            Some(JsonParseType::Array) => JsonType::Array(json_type_vec),
+            Some(JsonParseType::Object) | None => JsonType::Object(json_type_vec),
+        };
     }
 }
