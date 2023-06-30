310aa690c9c40f97aa0ec3de76aaa98a345fa4b5
Sandevik <62107622+Sandevik@users.noreply.github.com>
2023-06-30 11:49:03 +0200

    Json 3.0 changes

--- src/parser/json.rs
+++ src/parser/json.rs
@@ -1,187 +1,224 @@
-#![allow(dead_code, unused_assignments, unused_mut, unused_variables)]
-use crate::utils::Shift;
+#![allow(dead_code, unused_mut, unused_variables, unreachable_patterns)]
+use std::{collections::HashMap};
 
-impl<T> Shift<T> for Vec<T> {
-    fn shift(&mut self) -> ()
-    where
-        T: Clone,
-    {
-        *self = self[1..].to_vec();
-    }
-}
-
-#[derive(Debug, Clone)]
-pub enum VariableValue {
-    String(String),
+#[derive(Debug)]
+pub enum Value {
+    Int(i32),
     Float(f32),
-    Integer(i32),
-    Bool(bool),
-    None,
-    Array(Vec<VariableValue>),
-    Object(Vec<Variable>),
+    String(String),
+    True,
+    False,
+    Null,
 }
 
-#[derive(Debug, Clone)]
-pub struct Variable {
-    key: String,
-    value: VariableValue,
+#[derive(Debug)]
+pub enum VariableTypedValue {
+    Object(HashMap<String, VariableTypedValue>),
+    Array(Vec<VariableTypedValue>),
+    Value(Value)
 }
 
-pub struct Json {}
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenType {
+    Ident(String),
+    Colon,
+    Comma,
+    OpenCurly,
+    CloseCurly,
+    OpenBrack,
+    CloseBrack
+}
 
-impl Json {
-    pub fn parse(string: String) -> Vec<Variable> {
-        let mut bytes: Vec<u8> = string.bytes().collect::<Vec<u8>>();
-        let mut parsed: Vec<Variable> = Vec::new();
-        while bytes.len() > 0 {
-            let mut byte = bytes[0];
-            if byte == b'{' {
-                bytes.shift();
-                let object = Self::parse_string_to_object(&mut bytes, None);
-                parsed.push(object);
-            } else if byte == b'[' {
-                println!("parsing array");
-                bytes.shift();
-                let array = Self::parse_array(&mut bytes);
-                parsed.push(Variable {
-                    key: "".to_owned(),
-                    value: array,
-                });
-            }
-            bytes.shift();
+#[derive(Debug)]
+pub struct Consumer {
+    pub pos: usize,
+    pub ch: char,
+    pub size: usize,
+    vec: Vec<u8>
+}
+impl Consumer {
+    pub fn new(src: &str) -> Consumer {
+        return Consumer {
+            vec: src.bytes().collect::<Vec<u8>>(),
+            pos: 0,
+            ch: src.bytes().collect::<Vec<u8>>()[0] as char,
+            size: src.bytes().len(),
         }
-        return parsed;
     }
 
-    fn parse_variables(mut bytes: &mut Vec<u8>) -> Vec<Variable> {
-        let mut variables: Vec<Variable> = Vec::new();
-        let mut current_kv_pair: String = String::new();
-        let string: String = String::from_utf8(bytes.clone()).unwrap();
+    pub fn eat(&mut self) -> char {
+        if  self.pos + 1 < self.size {
+            self.pos += 1;
+            self.ch = self.vec[self.pos] as char;
+        } else {
+            self.pos = self.size;
+        }
+        return self.ch; 
+    }
 
-        /* TODO: Add compatability for nested objects */
-        /* TODO: Remove trailing commas in values? */
+    pub fn peek(&self) -> Result<char, String> {
+        if self.pos + 1 < self.size {
+            return Ok(self.vec[self.pos + 1] as char)
+        }else{
+            return Err("EOF / EOL".to_string())
+        }
+    }
+}
 
-        while bytes.len() >= 0 {
-            if bytes.len() == 0 {
-                variables.push(Self::parse_variable_from_kv(&current_kv_pair));
-                break;
-            }
-            let byte = bytes[0];
-            if byte == b'[' {
-                while bytes.len() > 0 && bytes[0] != b']' {  
-                    // this should become [1,2,34,5,6676]
-                    if !bytes[0].is_ascii_whitespace() {
-                        current_kv_pair.push(bytes[0] as char);
+pub struct Tokenizer {}
+impl Tokenizer {
+    pub fn tokenize(string: &str) -> Vec<TokenType>{
+        let mut token_types: Vec<TokenType> = Vec::new();
+        let mut value: String = String::new();
+        let mut consumer: Consumer = Consumer::new(string);
+        while consumer.pos < consumer.size{
+            match consumer.ch {
+                ',' => {
+                    if !value.is_empty() {
+                        token_types.push(TokenType::Ident(value.trim().to_string()));
+                        value = String::new();
                     }
-                    bytes.shift();
+                    token_types.push(TokenType::Comma);
+                    },
+                ':' => {
+                    if !value.is_empty() {
+                        token_types.push(TokenType::Ident(value.trim().to_string()));
+                        value = String::new();
+                    }
+                    token_types.push(TokenType::Colon)
+                    },
+                '{' => {
+                    token_types.push(TokenType::OpenCurly)
+                },
+                '}' => {
+                    if !value.is_empty() {
+                        token_types.push(TokenType::Ident(value.trim().to_string()));
+                        value = String::new();
+                    }
+                    token_types.push(TokenType::CloseCurly)
+                },
+                '[' => {
+                    token_types.push(TokenType::OpenBrack)
+                },
+                ']' => {
+                    if !value.is_empty() {
+                        token_types.push(TokenType::Ident(value.trim().to_string()));
+                        value = String::new();
+                    }
+                    token_types.push(TokenType::CloseBrack)
+                },
+                
+                ch => value.push(ch),
+            }
+            consumer.eat();
+        }
+        if !value.is_empty() {
+            token_types.push(TokenType::Ident(value));
+        }
+        return token_types
+    }
+}
+
+pub struct Parser {}
+impl Parser {
+
+    pub fn variablize(string: &str) {
+        let mut variables: Vec<VariableTypedValue> = Vec::new(); 
+        let mut tokens: Vec<TokenType> = Tokenizer::tokenize(string);
+        while tokens.len() > 0 {
+            let token = &tokens[0];
+            match token {
+                TokenType::OpenCurly => println!("{{ parse obj }}"), /*  variables.push(Self::parse_obj(&mut tokens)) */
+                TokenType::CloseCurly => println!("{{ obj ended }}"),
+                TokenType::OpenBrack => variables.push(Self::parse_arr(&mut tokens)),
+
+                TokenType::Ident(value) => println!("ident: {:?}", Self::parse_value_from_string(TokenType::Ident(value.clone())).unwrap()),
+                TokenType::Colon => println!("previous value was key, next is value"),
+                TokenType::Comma => println!("new variable"),
+                
+                token => println!("Token \"{:?}\" has not been matched yet", token)
+            }
+            tokens = tokens[1..].to_vec();
+        }
+        println!("{:?}", variables);
+    }
+
+    fn parse_arr(tokens: &mut Vec<TokenType>) -> VariableTypedValue {
+        let mut values: Vec<VariableTypedValue> = Vec::<VariableTypedValue>::new();
+        //consume the "[" to prevent infinite loop
+        *tokens = tokens[1..].to_vec();
+        while tokens.len() > 0 && tokens[0] != TokenType::CloseBrack {
+            let token = &tokens[0];
+            match token {
+                TokenType::Ident(val) => values.push(VariableTypedValue::Value(Self::parse_value_from_string(token.clone()).unwrap())),
+                _ => {}
+            }
+            *tokens = tokens[1..].to_vec();
+        }
+        //consume the "]" to prevent adding it as a variable
+        *tokens = tokens[1..].to_vec();
+        return VariableTypedValue::Array(values);
+    }
+
+    fn parse_obj(tokens: &mut Vec<TokenType>) -> VariableTypedValue {
+        let mut hashmap: HashMap<String, VariableTypedValue> = HashMap::new();
+        let mut key: String = String::new();
+        let mut value: VariableTypedValue = VariableTypedValue::Value(Value::Null);
+
+        //consume "{" to prevent infinate loop
+        *tokens = tokens[1..].to_vec();
+        while tokens.len() > 0 && tokens[0] != TokenType::CloseCurly {
+            let token = &tokens[0];
+
+            if *token != TokenType::OpenCurly {
+                if key.is_empty(){
+                    match token {
+                        TokenType::Ident(val) => key = val.clone(),
+                        _ => {}
+                    }
+                } else {
+                    match token {
+                        TokenType::Ident(val) => value = VariableTypedValue::Value(Self::parse_value_from_string(token.clone()).unwrap()),
+                        _ => {}
+                    }
+                    hashmap.insert(key, value);
+                    key = String::new();
+                    value = VariableTypedValue::Value(Value::Null);
                 }
-                current_kv_pair.push(']');
+            }
             
-            } else if (byte == b',' && !String::is_empty(&current_kv_pair)) || byte == b']' {
-                variables.push(Self::parse_variable_from_kv(&current_kv_pair));
-                current_kv_pair = String::new();
-            } else{
-                current_kv_pair.push(byte as char);
-            }
-
-            bytes.shift();
+            
+            *tokens = tokens[1..].to_vec();
         }
+        //consume the "}" to prevent adding it as a variable
+        *tokens = tokens[1..].to_vec();
 
-        return variables
+
+        VariableTypedValue::Object(hashmap)
     }
 
-    fn parse_variable_from_kv(str: &str) -> Variable {
-        let key = &Self::clean_string(str.split(":").collect::<Vec<&str>>()[0].trim());
-        let value_string = str.split(":").collect::<Vec<&str>>()[1];
-        let var: Variable = Variable {
-            key: key.to_string(),
-            value: Self::parse_variable_value(value_string),
-        };
-        return var;
-    }
-
-    fn parse_variable_value(value_string: &str) -> VariableValue {
-        let mut bytes: Vec<u8> = value_string.clone().bytes().collect::<Vec<u8>>();
-        let value = match value_string.trim() {
-            "null" => VariableValue::None,
-            "true" => VariableValue::Bool(true),
-            "false" => VariableValue::Bool(false),
-            str if str.contains("[") => VariableValue::Array(Self::parse_array_values(&mut bytes)),
-            str if str.contains("\"") => {
-                VariableValue::String(Self::clean_string(value_string.trim()).to_string())
-            }
-            str if str.contains(".") => VariableValue::Float(
-                value_string
-                    .trim()
-                    .parse::<f32>()
-                    .expect(&format!("Could not parse int: {}", value_string)),
-            ),
-            _ => VariableValue::Integer(
-                value_string
-                    .trim()
-                    .parse::<i32>()
-                    .expect(&format!("Could not parse int: {}", value_string)),
-            ),
-        };
-        return value;
-    }
-
-    fn parse_array(mut bytes: &mut Vec<u8>) -> VariableValue {
-        let mut array: Vec<_> = vec![];
-        let mut current_value: String = String::new();
-        while bytes.len() > 0 {
-            if bytes[0] == b',' {
-                array.push(Self::parse_variable_value(&current_value))
-            } else {
-                current_value.push(bytes[0] as char);
-            }
-            bytes.shift();
-        }
-        return VariableValue::Array(array);
-    }
-
-    fn parse_array_values(mut bytes: &mut Vec<u8>) -> Vec<VariableValue> {
-        let mut values: Vec<VariableValue> = Vec::new();
-        let mut current_value: String = String::new();
-        while bytes.len() > 0 {
-            if bytes[0] != b'[' && bytes[0] != b',' && bytes[0] != b']' {
-                current_value.push(bytes[0] as char); 
-            }else if bytes[0] == b',' || bytes[0] == b']' {
-                values.push(Self::parse_variable_value(&current_value));
-                current_value = String::new();
-            }
-            bytes.shift();
-        }
-        return values;
-    }
-
-    fn parse_string_to_object(mut bytes: &mut Vec<u8>, key: Option<String>) -> Variable {
-        let mut variables: Vec<Variable> = Vec::new();
-        let mut var: Variable;
-        let mut nested_string = String::new();
-        while bytes.len() > 0 && bytes[0] != b'}' {
-            nested_string.push(bytes[0] as char);
-            bytes.shift();
-        }
-        let obj_vec = Self::parse_variables(&mut nested_string.bytes().collect::<Vec<u8>>());
-        return match key {
-            Some(key) => Variable {
-                key: key,
-                value: VariableValue::Object(obj_vec),
+    fn parse_value_from_string(token: TokenType) -> Result<Value, String> {
+        match token {
+            TokenType::Ident(value) => {
+                match value.as_str() {
+                    "null" => Ok(Value::Null),
+                    "true" => Ok(Value::True),
+                    "false" => Ok(Value::False),
+                    str if str.contains("\"") || str.bytes().filter(|x| *x == b'.').collect::<Vec<u8>>().len() > 1 => Ok(Value::String(Self::clean_string(str))),
+                    str if str.contains(".") => Ok(Value::Float(str.parse::<f32>().unwrap())),
+                    str => Ok(Value::Int(str.parse::<i32>().unwrap())),
+                    str => {
+                        Err(format!("Could not parse value: \"{}\"", str))
+                    }
+                }
             },
-            None => Variable {
-                key: "".to_string(),
-                value: VariableValue::Object(obj_vec),
-            },
-        };
+            t => Err(format!("Token value cannot be parsed, {:?}", t))
+        }
     }
 
     fn clean_string(str: &str) -> String {
-        return String::from_utf8(
-            str.bytes()
-                .filter(|char| *char != b'"' || *char != b'\"')
-                .collect::<Vec<u8>>(),
-        )
-        .unwrap();
+        str.chars().filter(|ch| *ch != '\"').collect::<String>()
     }
 }
+
+
