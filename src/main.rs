5552a35004868274cf20e3b0bff1957d2c108401
[Simon] <[simon.sandevik@outlook.com]>
2023-07-19 18:49:24 +0200

    parse elements, attrs and tag parsing not yet implemented

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::fs;
+
 use parser::xml_html_php::Consumer;
 
-use crate::parser::xml_html_php::Tokenizer;
+use crate::parser::xml_html_php::Parser;
 
 //use std::fs;
 mod parser;
@@ -8,12 +10,12 @@ mod utils;
 
 
 fn main() {
-
+    let index = fs::read_to_string("index.html").unwrap();
     let html = "<!DOCTYPE html><html class=\"\" [sdadsa] sadads-fsdaff ><?php $test = \"asdad\" ?><!-- test --><div>content abc 123</div><img /></html>"; //fs::read_to_string("index.html").expect("File not found");
 
     //println!("{:#?}", Json::parse("{\"firstName\" : \"John\",\"lastName\" : \"Doe\",\"age\" : 23,\"residency\" : {\"address\" : \"One Way 21\",\"zip\" : 123567,\"city\" : \"Big City\"},\"pets\" : [{\"animal\" : \"cat\", \"age\" : 2, \"name\" : \"Tom\"}, {\"animal\" : \"mouse\", \"age\" : 1, \"name\" : \"Jerry\"}],\"lastCoordinates\" : [[\"lat 84.45369\", \"long 12.5467\"], [\"lat 55.255657\", \"long 67.35677\"]]}"));
 
-    let mut consumer: Consumer = Consumer::from(html);
-    let mut tokens = Tokenizer::tokenize(consumer);
+    let mut consumer: Consumer = Consumer::from(&index);
+    let mut tokens = Parser::parse_elements(consumer);
     println!("{:#?}", tokens);
 }
--- src/parser/xml_html_php.rs
+++ src/parser/xml_html_php.rs
@@ -1,13 +1,13 @@
 #![allow(dead_code, unused_variables, unused_assignments, unused_mut)]
 
-use std::rc::Rc;
+use std::{collections::HashMap, rc::Rc};
 
 #[derive(Debug)]
 pub struct Consumer {
     buf: Rc<[u8]>,
     pos: usize,
     size: usize,
-    ch: char
+    ch: char,
 }
 impl Consumer {
     pub fn from(str: &str) -> Consumer {
@@ -15,7 +15,7 @@ impl Consumer {
             buf: str.bytes().collect::<Rc<[u8]>>(),
             pos: 0,
             size: str.bytes().len(),
-            ch: str.chars().nth(0).unwrap()
+            ch: str.chars().nth(0).unwrap(),
         }
     }
 
@@ -23,21 +23,21 @@ impl Consumer {
         if self.pos + 1 < self.size {
             self.pos += 1;
             self.ch = self.buf[self.pos] as char;
-        }else{
+        } else {
             self.pos = self.size;
         }
-        return self.ch
+        return self.ch;
     }
     pub fn peek(&self) -> char {
         if self.pos + 1 < self.size {
             return self.buf[self.pos + 1] as char;
-        }else {
+        } else {
             return 0 as char;
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Token {
     Doctype(String),
     XML(String),
@@ -46,8 +46,8 @@ pub enum Token {
     EndTag(String),
     SelfClosing(String),
     Comment(String),
-    String(String),
-    EOF
+    Content(String),
+    EOF,
 }
 enum TokenType {
     Doctype,
@@ -58,32 +58,88 @@ enum TokenType {
     EOF,
     Unknown,
     XML,
-    PHP
+    PHP,
 }
 
-pub struct Tokenizer {}
-impl Tokenizer {
+#[derive(Debug, PartialEq)]
+pub enum Tag {
+    Doctype,
+    XML,
+
+    Html,
+    Head,
+    Body,
+    Meta,
+    Title,
+
+    Div,
+    Span,
+    P,
+    H1,
+    H2,
+    H3,
+    H4,
+    H5,
+    H6,
+
+    Ul,
+    Ol,
+    Li,
+
+    Img,
+
+    PHP,
+    Unknown,
+}
+
+pub type Attributes = Vec<HashMap<String, String>>;
+
+#[derive(Debug, PartialEq)]
+pub struct Element {
+    tag: Tag,
+    content: Option<String>,
+    attr: Option<Attributes>,
+}
+impl Element {
+    pub fn default() -> Element {
+        Element {
+            tag: Tag::Unknown,
+            content: None,
+            attr: None,
+        }
+    }
+
+    pub fn new(tag: Tag, content: Option<String>, attr: Option<Attributes>) -> Element {
+        Element {
+            tag: tag,
+            content: content,
+            attr: attr,
+        }
+    }
+}
+
+pub struct Parser {}
+impl Parser {
     pub fn tokenize(mut consumer: Consumer) -> Vec<Token> {
         let mut tokens: Vec<Token> = Vec::<Token>::new();
         let mut token_type: TokenType = TokenType::Unknown;
         let mut ident: String = String::new();
-
         while consumer.pos < consumer.size {
             match consumer.ch {
                 '<' => {
                     if ident.len() > 0 {
-                        tokens.push(Token::String(ident));
+                        tokens.push(Token::Content(ident));
                         ident = String::new();
                     }
                     if consumer.peek() == '/' {
                         //closing tag
                         token_type = TokenType::EndTag;
                         ident.push(consumer.ch);
-                    }else if consumer.peek() == '!' {
+                    } else if consumer.peek() == '!' {
                         if consumer.buf[consumer.pos + 2] == b'D' {
                             //Doctype
                             token_type = TokenType::Doctype;
-                        }else{
+                        } else {
                             // comment
                             token_type = TokenType::Comment;
                         }
@@ -91,13 +147,14 @@ impl Tokenizer {
                     } else if consumer.peek() == '?' {
                         //xml declatation
                         /* TODO: Add functionality to parse PHP */
-                        if consumer.buf[consumer.pos + 2] != b'p' && consumer.buf[consumer.pos + 2] != b'=' {
+                        if consumer.buf[consumer.pos + 2] != b'p'
+                            && consumer.buf[consumer.pos + 2] != b'='
+                        {
                             token_type = TokenType::XML
                         } else {
                             token_type = TokenType::PHP
                         }
                         ident.push(consumer.ch);
-
                     } else {
                         // opening tag
                         token_type = TokenType::StartTag;
@@ -109,7 +166,7 @@ impl Tokenizer {
                         //self closing
                         token_type = TokenType::SelfClosing;
                         ident.push(consumer.ch);
-                    }else{
+                    } else {
                         ident.push(consumer.ch);
                     }
                 }
@@ -124,22 +181,68 @@ impl Tokenizer {
                         TokenType::SelfClosing => Token::SelfClosing(copy_ident),
                         TokenType::Comment => Token::Comment(copy_ident),
                         TokenType::EOF => Token::EOF,
-                        TokenType::Unknown => Token::String(copy_ident),
+                        TokenType::Unknown => Token::Content(copy_ident),
                         TokenType::PHP => Token::PHP(copy_ident),
                     };
                     tokens.push(token);
                     ident = String::new();
                 }
+                _ => ident.push(consumer.ch),
+            }
+            consumer.eat();
+        }
+        return tokens;
+    }
 
-                
-                _ => ident.push(consumer.ch)
+    pub fn parse_elements(consumer: Consumer) -> Vec<Element> {
+        let mut elements = Vec::<Element>::new();
+        let mut tokens = Self::tokenize(consumer);
+        let mut element: Element = Element::default();
+
+        while tokens.len() > 0 {
+            match &tokens[0] {
+                Token::Doctype(str) => element = Element {tag: Tag::Doctype, content: None, attr: Self::parse_attributes(str)},
+                Token::Content(str) => element = Element {tag: element.tag, content: Some(str.to_string()), attr: element.attr},
+                Token::StartTag(str) => element = Element {tag: Self::parse_tag(str), content: None, attr: Self::parse_attributes(str)},
+                Token::EndTag(str) => {
+                    elements.push(element);
+                    element = Element::default();
+                }
+                Token::SelfClosing(str) => {
+                    if element != Element::default() {
+                        elements.push(element);
+                        element = Element::default();
+                    }
+                    elements.push(Element::new(Self::parse_tag(str), None, Self::parse_attributes(str)));
+                }
+                Token::PHP(str) => elements.push(Self::parse_php_tag(str)),
+                Token::XML(str) => elements.push(Element::new(Tag::XML, None, Self::parse_attributes(str))),
+
+                Token::Comment(str) => {}
+                Token::EOF => {}
             }
 
-            consumer.eat();
-        
+
+
+
+            tokens = tokens[1..].to_vec();
         }
-
-        return tokens;
-
+        return elements;
     }
+
+
+    fn parse_attributes(str: &str) -> Option<Attributes> {
+
+        return None;
+    }
+
+    fn parse_tag(str: &str) -> Tag {
+        return Tag::Unknown;
+    }
+
+    fn parse_php_tag(str: &str) -> Element {
+
+        return Element::default();
+    }
+    
 }
